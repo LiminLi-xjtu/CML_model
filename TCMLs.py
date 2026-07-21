4a123a66812ba4a10b457e9d6c51a09e6db2c594
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2026-07-21 15:04:28 +0800

    Add files via upload

---
+++ TCMLs.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from model import TCML
+from utiles import load_and_process_data, stage_dataset_params
+
+if __name__ == '__main__':
+    dataset = 'citeseer'  # 'cora', 'citeseer' or 'acm'
+    stage = 3   # stage>=2
+    A, X, gnd = load_and_process_data(dataset, stage)
+    params = stage_dataset_params[dataset][stage]
+
+    Z, acc, nmi, F1 = TCML(
+        A, X, gnd,
+        eta=params["eta"],
+        alpha=params["alpha"],
+        beta=params["beta"],
+        lamb=params["lamb"],
+        t=params["t"],
+        max_iter=params["max_iter"])
